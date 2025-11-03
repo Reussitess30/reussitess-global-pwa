fa6352fd94ae6302db891175838e949e6911212a
Votre Nom d'Utilisateur GitLab <Votre Email GitLab>
2025-11-03 06:36:34 -0400

    Ajout du fichier pages/index.js manquant pour la page d'accueil

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Bienvenue sur Reussitess® Global PWA !</h1>
+      <p>L'application est correctement déployée et la PWA est active.</p>
+    </div>
+  );
+};
+
+export default HomePage;
